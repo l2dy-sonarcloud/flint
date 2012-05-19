eb4f03cad7ded92db43dd6772099621f98c7f304
james <james@noemail.net>
2012-05-19 15:50:07 +0000

    Use text since it's the resulting datatype used by sqlite anyways
    FossilOrigin-Name: d795104fa6ae3e2cdcc4562019fba1978baf02cb

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE repositories (
     id integer primary key autoincrement,
     user_id integer,
-    name varchar(255),
+    name text,
     private integer,
     cloned integer,
     auto_update integer
@@ -9,22 +9,22 @@ CREATE TABLE repositories (
 
 CREATE TABLE sessions (
     user_id integer primary key,
-    session_id varchar(255),
+    session_id text,
     session_date text
 );
 
 CREATE TABLE tokens (
     user_id integer primary key,
-    token varchar(255),
+    token text,
     create_date text
 );
 
 CREATE TABLE users (
     id integer primary key autoincrement,
-    first_name varchar(255),
-    last_name varchar(255),
-    email varchar(255),
-    username varchar(255),
-    password varchar(255),
-    salt varchar(255)
+    first_name text,
+    last_name text,
+    email text,
+    username text,
+    password text,
+    salt text
 );
