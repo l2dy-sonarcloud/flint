05dab6421116b4d364e1bc31ba6fd37b810c89c3
rkeene <rkeene@noemail.net>
2013-05-03 13:47:31 +0000

    Removed use of "sudo" in rebuild script
    FossilOrigin-Name: 99ae108d6c0466e88c28b114e65ff5a35a4c6b3f

--- scripts/rebuild.php
+++ scripts/rebuild.php
@@ -4,6 +4,6 @@ $repositories = glob('../repos/*/*.fossil');
 
 foreach ($repositories as $repository) {
     echo $repository . "\n";
-    system("sudo fossil rebuild -R {$repository}");
+    system("fossil rebuild -R {$repository}");
     echo "\n";
 }
